7d7cc4c6d1829a261f9887879902992be70f036c
erin.miller <erinm1018@gmail.com>
2024-10-15 12:06:28 -0500

    add: auth and connected

--- app/page.tsx
+++ app/page.tsx
@@ -35,18 +35,17 @@ const HomePage: React.FC = () => {
     };
     
     const initiateOAuth = () => {
-        // const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-        // const redirectUri = encodeURIComponent('https://scouttoon.info/');
-        // const scope = encodeURIComponent('identify');
+        const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+        const redirectUri = encodeURIComponent('https://scouttoon.info/');
+        const scope = encodeURIComponent('identify');
 
-        // const randomState = generateRandomString();
-        // localStorage.setItem('oauth-state', randomState);
+        const randomState = generateRandomString();
+        localStorage.setItem('oauth-state', randomState);
 
-        // const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}&state=${btoa(randomState)}`;
+        const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}&state=${btoa(randomState)}`;
         
-        // // Redirect to Discord authorization URL
-        // window.location.href = url;
-        setIsAuth(true);
+        // Redirect to Discord authorization URL
+        window.location.href = url;
     };
 
     useEffect(() => {
@@ -61,12 +60,12 @@ const HomePage: React.FC = () => {
     }, []);
 
     return (
-        <div className="h-screen flex justify-center items-center bg-gags-pattern bg-repeat">
+        <div className="h-screen flex justify-center items-center bg-gags-pattern bg-repeat overflow-y-auto">
             {/* If user is not authenticated, show the authentication modal */}
             {!isAuth && (
                 <div className="fixed inset-0 flex justify-center items-center z-50">
-                    <div className="bg-white p-10 rounded-lg shadow-lg text-center border border-gray-300"> {/* Added border classes */}
-                        <h2 className="text-2xl font-semibold font-minnie text-gray-800 mb-6">Connect to Discord</h2>
+                    <div className="bg-white p-10 rounded-lg shadow-lg text-center border border-gray-300">
+                        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-6">Connect to Discord</h2>
                         <p className="text-xl text-gray-600 font-impress">ToonScout needs a Discord connection to function.</p>
                         <p className="text-xl text-gray-600 font-impress">Click the button below to begin!</p>
                         <br />
@@ -80,8 +79,8 @@ const HomePage: React.FC = () => {
                             style={{
                                 backgroundImage: `url(${isPressed ? clickedImg : unclickedImg})`,
                                 backgroundSize: 'contain',
-                                backgroundPosition: 'center', // Center the image
-                                cursor: 'pointer', // Change cursor to pointer for better UX
+                                backgroundPosition: 'center',
+                                cursor: 'pointer',
                             }}
                         >
                         </button>
@@ -93,17 +92,19 @@ const HomePage: React.FC = () => {
             {isAuth && !isConnected && (
                 <div className="fixed inset-0 flex justify-center items-center bg-gags-pattern bg-repeat">
                     <div className="bg-white p-10 rounded-lg shadow-lg text-center space-y-6 border border-gray-300">
-                        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-4">Final steps...</h2>
-                        {/* Step 1 */}
-                        <div className="bg-gray-100 p-6 rounded-lg shadow-md border border-gray-300">
-                            <h2 className="text-xl font-semibold font-minnie text-gray-800 mb-4">Enable Companion App Support</h2>
-                            <img src='/images/gameplay-menu.png' alt='Gameplay menu' className="mx-auto" />
-                        </div>
+                        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-4">Connecting to Toontown Rewritten...</h2>
+                        <div className="flex justify-center space-x-8">
+                            {/* Step 1 */}
+                            <div className="bg-gray-100 p-6 rounded-lg shadow-md border border-gray-300">
+                                <h2 className="text-2xl font-semibold font-minnie text-gray-800 mb-4">1. Enable Companion App Support</h2>
+                                <img src='/images/gameplay-menu.png' alt='Gameplay menu' className="mx-auto" />
+                            </div>
 
-                        {/* Step 2 */}
-                        <div className="bg-gray-100 p-6 rounded-lg shadow-md border border-gray-300">
-                            <h2 className="text-xl font-semibold font-minnie text-gray-800 mb-4">Click "OK" on in-game popup</h2>
-                            <img src='' alt='Step 2 Image' className="mx-auto" />
+                            {/* Step 2 */}
+                            <div className="bg-gray-100 p-6 rounded-lg shadow-md border border-gray-300">
+                                <h2 className="text-2xl font-semibold font-minnie text-gray-800 mb-12">2. Click "OK" on in-game popup</h2>
+                                <img src='/images/prompt.png' alt='Prompt' className="mx-auto" />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -111,10 +112,49 @@ const HomePage: React.FC = () => {
 
             {/* If user is authenticated and connected, show home screen */}
             {isAuth && isConnected && (
-                <div className="fixed inset-0 flex justify-center items-center bg-gags-pattern bg-repeat">
-                    <div className="bg-white p-10 rounded-lg shadow-lg text-center border border-gray-300">
-                        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Welcome to ToonScout!</h2>
-                        <p className="text-lg text-gray-600">To connect, follow the steps below.</p>
+                <div className="relative h-screen overflow-hidden">
+                    <div className="bg-white p-6 md:p-10 rounded-lg shadow-lg text-center border border-gray-300 w-full max-w-6xl mx-auto space-y-6 md:space-y-8 mt-10 overflow-y-auto">
+                        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-4">Welcome to ToonScout!</h2>
+                        <p className="text-xl text-gray-600 font-impress mb-6">Run these commands anywhere in Discord to get real-time information regarding your toon!</p>
+
+                        {/* Commands Container */}
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+                            {/* /info command */}
+                            <div className="bg-gray-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/info</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Displays your toon photo, laff, and location.</p>
+                            </div>
+
+                            {/* /fish command */}
+                            <div className="bg-blue-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/fish</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Provides advising on where to fish or what to catch.</p>
+                            </div>
+
+                            {/* /suit command */}
+                            <div className="bg-red-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/suit</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Get your suit progress and promotion recommendations.</p>
+                            </div>
+
+                            {/* /gags command */}
+                            <div className="bg-purple-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/gags</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Displays current gags and progress.</p>
+                            </div>
+
+                            {/* /tasks command */}
+                            <div className="bg-green-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/tasks</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Lists your active toontasks and progress.</p>
+                            </div>
+
+                            {/* /support command */}
+                            <div className="bg-pink-100 p-4 md:p-6 rounded-lg shadow-md border border-gray-300">
+                                <h3 className="text-xl font-semibold font-minnie text-gray-800 mb-2 md:mb-4">/support</h3>
+                                <p className="text-lg text-gray-600 font-impress mb-2">Get a Discord link to our support server.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
