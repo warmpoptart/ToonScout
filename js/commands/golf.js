7917c35dfa945ae2e660be35a38757f0d1a61e03
erin.miller <erinm1018@gmail.com>
2024-10-30 23:32:18 -0500

    show: all golf/race trophies in diff embeds

--- js/commands/golf.js
+++ js/commands/golf.js
@@ -1,4 +1,9 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { 
+    SlashCommandBuilder, 
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder 
+} from 'discord.js';
 import { InteractionResponseType } from 'discord-interactions';
 import { getToonRendition } from '../utils.js';
 import { getScoutToken } from '../db/scoutData/scoutService.js';
@@ -20,23 +25,150 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(req, res, target) {
     const item = await getScoutToken(target);
-    const toon = item.data;
-    
-    const embed = new EmbedBuilder()
-        .setColor('Green')
-        .setAuthor({ name: toon.toon.name, iconURL: getToonRendition(toon, 'laffmeter') })
-        .setTitle(`Golf Trophies (${getTotalEarned(toon.golf)}/30)`)
-        .setThumbnail(pencil)
-        .setDescription(getTrophies(toon.golf))
-        .setFooter({ text: getLaff(toon.golf), iconURL: trophyIcon})
-        .setTimestamp(item.modified)
+
+    const row = new ActionRowBuilder()
+    .addComponents(
+        getRefreshButton('home', target),
+        getAdviceButton(target)
+    )
 
     return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-            embeds: [embed]
+            embeds: [getHomeEmbed(item)],
+            components: [row]
         }
     });
+}
+
+export async function handleButton(req, customId) {
+    let embed;
+    let row;
+    let target;
+    let state;
+    
+    const parts = customId.split(':');
+    const action = parts[0];
+
+    if (parts.length === 3) {
+        state = parts[1];
+        target = parts[2];
+    } else {
+        state = null;
+        target = parts[1];
+    }
+	
+    const item = await getScoutToken(target);
+
+    switch (action) {
+        case 'golf-refresh':
+            switch (state) {
+                case 'home':
+                    embed = getHomeEmbed(item);
+                    row = getHomeRow(target);
+                    break;
+                case 'advice':
+                    embed = getAdviceEmbed(item);
+                    row = getAdviceRow(target);
+                    break;
+                default:
+                    return;
+            }
+            break;
+        case 'golf-home':
+            embed = getHomeEmbed(item)
+            row = getHomeRow(target)
+            break;
+        case 'golf-advice':
+            embed = getAdviceEmbed(item);
+            row = getAdviceRow(target);
+            break;
+        default:
+            return;
+    }
+
+    return { embed, row };
+}
+
+function getHomeButton(target) {
+    return new ButtonBuilder()
+        .setCustomId(`golf-home:${target}`)
+        .setLabel('Home')
+        .setStyle('Secondary')
+}
+
+function getAdviceButton(target) {
+    return new ButtonBuilder()
+        .setCustomId(`golf-advice:${target}`)
+        .setLabel('Advice')
+        .setStyle('Secondary')
+}
+
+function getRefreshButton(type, target) {
+    return new ButtonBuilder()
+        .setCustomId(`golf-refresh:${type}:${target}`)
+        .setLabel('Refresh')
+        .setStyle('Danger')
+}
+
+function getHomeRow(target) {
+    return new ActionRowBuilder()
+        .addComponents(
+            getRefreshButton('home', target),
+            getAdviceButton(target)
+        );
+}
+
+function getAdviceRow(target) {
+    return new ActionRowBuilder()
+        .addComponents(
+            getRefreshButton('advice', target),
+            getHomeButton(target)
+        );
+}
+
+function getHomeEmbed(item) {
+    const toon = item.data;
+    const trophies = getAllTrophies(toon.golf);
+    const embed = new EmbedBuilder()
+        .setColor('Green')
+        .setAuthor({ name: toon.toon.name, iconURL: getToonRendition(toon, 'laffmeter') })
+        .setTitle(`Golfing Trophies (${getTotalEarned(toon.golf)}/30)`)
+        .setThumbnail(pencil)
+        .setFooter({ text: getLaff(toon.golf), iconURL: trophyIcon})
+        .setTimestamp(item.modified)
+
+    if (trophies) {
+        let trophyNames = ``;
+        let progNames = ``;
+
+        trophies.forEach(trophy => {
+            const progress = `${trophy.progress.current}/${trophy.progress.required}\n`;
+            trophyNames += `${trophy.name}\n`;
+            progNames += progress;
+        });
+
+        embed.addFields(
+            { name: 'Trophies', value: trophyNames, inline: true},
+            { name: 'Progress', value: progNames, inline: true}
+        );
+    } else {
+        embed.addDescription('You have maxed golfing! Congratulations!');
+    }
+
+    return embed;
+}
+
+function getAdviceEmbed(item) {
+    const toon = item.data;
+    return new EmbedBuilder()
+        .setColor('Green')
+        .setAuthor({ name: toon.toon.name, iconURL: getToonRendition(toon, 'laffmeter') })
+        .setTitle(`Golf Trophies (${getTotalEarned(toon.golf)}/30)`)
+        .setDescription(getTopTrophies(toon.golf))
+        .setThumbnail(pencil)
+        .setFooter({ text: getLaff(toon.golf), iconURL: trophyIcon})
+        .setTimestamp(item.modified)
 }
 
 function getLaff(toon) {
@@ -45,16 +177,19 @@ function getLaff(toon) {
     return `${pts}/3 laff points earned.`;
 }
 
-function getTrophies(toon) {
-    if (getTotalEarned(toon) === 30) {
-        return 'You have maxed golfing! Congratulations!';
-    }
+function getTopTrophies(toon) {
     const calc = new GolfCalculator(JSON.stringify(toon));
     let trophies = calc.getBestTrophy().slice(0,5);
     trophies = trophies.map((t, index) => 
         `**${index+1}. ${t.name}**Progress: ${t.progress.current}/${t.progress.required}\n${t.progress.difference} more to go!\n`
     ).join('\n');
-    return trophies;
+    return trophies !== '' ? trophies : 'You have maxed golfing! Congratulations!';
+}
+
+function getAllTrophies(toon) {
+    const calc = new GolfCalculator(JSON.stringify(toon));
+    const trophies = calc.getBestTrophy();
+    return trophies !== '' ? trophies : null;
 }
 
 function getTotalEarned(toon) {
--- js/commands/race.js
+++ js/commands/race.js
@@ -1,4 +1,9 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { 
+    SlashCommandBuilder, 
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder 
+} from 'discord.js';
 import { InteractionResponseType } from 'discord-interactions';
 import { getToonRendition } from '../utils.js';
 import { getScoutToken } from '../db/scoutData/scoutService.js';
@@ -20,24 +25,152 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(req, res, target) {
     const item = await getScoutToken(target);
-    const toon = item.data;
 
+    const row = new ActionRowBuilder()
+        .addComponents(
+            getRefreshButton('home', target),
+            getAdviceButton(target)
+        )
+
+    return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+            embeds: [getHomeEmbed(item)],
+            components: [row]
+        }
+    });
+}
+
+export async function handleButton(req, customId) {
+    let embed;
+    let row;
+    let target;
+    let state;
+    
+    const parts = customId.split(':');
+    const action = parts[0];
+
+    if (parts.length === 3) {
+        state = parts[1];
+        target = parts[2];
+    } else {
+        state = null;
+        target = parts[1];
+    }
+	
+    const item = await getScoutToken(target);
+
+    switch (action) {
+        case 'race-refresh':
+            switch (state) {
+                case 'home':
+                    embed = getHomeEmbed(item);
+                    row = getHomeRow(target);
+                    break;
+                case 'advice':
+                    embed = getAdviceEmbed(item);
+                    row = getAdviceRow(target);
+                    break;
+                default:
+                    return;
+            }
+            break;
+        case 'race-home':
+            embed = getHomeEmbed(item)
+            row = getHomeRow(target)
+            break;
+        case 'race-advice':
+            embed = getAdviceEmbed(item);
+            row = getAdviceRow(target);
+            break;
+        default:
+            return;
+    }
+
+    return { embed, row };
+}
+
+function getHomeButton(target) {
+    return new ButtonBuilder()
+        .setCustomId(`race-home:${target}`)
+        .setLabel('Home')
+        .setStyle('Secondary')
+}
+
+function getAdviceButton(target) {
+    return new ButtonBuilder()
+        .setCustomId(`race-advice:${target}`)
+        .setLabel('Advice')
+        .setStyle('Secondary')
+}
+
+function getRefreshButton(type, target) {
+    return new ButtonBuilder()
+        .setCustomId(`race-refresh:${type}:${target}`)
+        .setLabel('Refresh')
+        .setStyle('Danger')
+}
+
+function getHomeRow(target) {
+    return new ActionRowBuilder()
+        .addComponents(
+            getRefreshButton('home', target),
+            getAdviceButton(target)
+        );
+}
+
+function getAdviceRow(target) {
+    return new ActionRowBuilder()
+        .addComponents(
+            getRefreshButton('advice', target),
+            getHomeButton(target)
+        );
+}
+
+function getHomeEmbed(item) {
+    const toon = item.data;
+    const trophies = getAllTrophies(toon.racing);
     const embed = new EmbedBuilder()
         .setColor('Gold')
         .setAuthor({ name: toon.toon.name, iconURL: getToonRendition(toon, 'laffmeter') })
         .setTitle(`Racing Trophies (${getTotalEarned(toon.racing)}/30)`)
         .setThumbnail(car)
-        .setDescription(getTrophies(toon.racing))
         .setFooter({ text: getLaff(toon.racing), iconURL: trophyIcon})
         .setTimestamp(item.modified)
 
-    return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-            embeds: [embed]
-        }
-    });
+    if (trophies) {
+        let trophyNames = ``;
+        let progNames = ``;
+
+        trophies.forEach(trophy => {
+            const progress = `${trophy.progress.current}/${trophy.progress.required}\n`;
+            trophyNames += `${trophy.name}\n`;
+            progNames += progress;
+        });
+
+        embed.addFields(
+            { name: 'Trophies', value: trophyNames, inline: true},
+            { name: 'Progress', value: progNames, inline: true}
+        );
+    } else {
+        embed.addDescription('You have maxed racing! Congratulations!');
+    }
+
+    return embed;
 }
+
+function getAdviceEmbed(item) {
+    const toon = item.data;
+    return new EmbedBuilder()
+        .setColor('Gold')
+        .setAuthor({ name: toon.toon.name, iconURL: getToonRendition(toon, 'laffmeter') })
+        .setTitle(`Racing Trophies (${getTotalEarned(toon.racing)}/30)`)
+        .setDescription(getTopTrophies(toon.racing))
+        .setThumbnail(car)
+        .setFooter({ text: getLaff(toon.racing), iconURL: trophyIcon})
+        .setTimestamp(item.modified)
+}
+
 
 function getLaff(toon) {
     const calc = new RacingCalculator(JSON.stringify(toon));
@@ -45,13 +178,19 @@ function getLaff(toon) {
     return `${pts}/3 laff points earned.`;
 }
 
-function getTrophies(toon) {
+function getTopTrophies(toon) {
     const calc = new RacingCalculator(JSON.stringify(toon));
     let trophies = calc.getBestTrophy().slice(0,5);
     trophies = trophies.map((t, index) => 
         `**${index+1}. ${t.name}**Progress: ${t.progress.current}/${t.progress.required}\n${t.progress.difference} more to go!\n`
     ).join('\n');
     return trophies !== '' ? trophies : 'You have maxed racing! Congratulations!';
+}
+
+function getAllTrophies(toon) {
+    const calc = new RacingCalculator(JSON.stringify(toon));
+    const trophies = calc.getBestTrophy();
+    return trophies !== '' ? trophies : null;
 }
 
 function getTotalEarned(toon) {
