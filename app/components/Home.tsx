021e71866291368a45c88667c94336b29a8da10b
Erin Miller <erinm1018@gmail.com>
2024-11-01 19:01:22 +0000

    fix: gag color hex code was improperly formatted

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -6,7 +6,7 @@ const commands = [
     { title: '/info', description: 'Displays your toon photo, laff, and location.', color: '#4A90E2' },
     { title: '/fish', description: 'Provides advising on where to fish or what to catch.', color: '#4c7fe2' },
     { title: '/suit', description: 'Get suit progress and recommendations.', color: '#4d6de2' },
-    { title: '/gags', description: 'Displays current gags and progress.', color: '#7504AE2' },
+    { title: '/gags', description: 'Displays current gags and progress.', color: '#504AE2' },
     { title: '/tasks', description: 'Lists your active toontasks and progress.', color: '#764AE2' },
     { title: '/race', description: 'Provides advising on racing trophies.', color: '#9C4AE2'},
     { title: '/golf', description: 'Provides advising on golfing trophies.', color: '#BF4ADF'},
