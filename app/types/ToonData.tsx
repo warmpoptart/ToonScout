9e421757ef77eee39e8847c1d23c6a2489b8d517
erin.miller <erinm1018@gmail.com>
2024-12-24 14:07:18 -0600

    types: add ToonData

---
+++ app/types/ToonData.tsx
@@ -0,0 +1,133 @@
+interface GagExperience {
+  current: number;
+  next: number;
+}
+
+interface Gag {
+  gag: {
+    level: number;
+    name: string;
+  };
+  organic: any;
+  experience: GagExperience;
+}
+
+interface Toon {
+  id: string;
+  name: string;
+  species: string;
+  headColor: string;
+  style: string;
+}
+
+interface Location {
+  zone: string;
+  neighborhood: string;
+  district: string;
+  instanceId: number;
+}
+
+interface TaskProgress {
+  text: string;
+  current: number;
+  target: number;
+}
+
+interface Task {
+  objective: {
+    text: string;
+    where: string;
+    progress: TaskProgress;
+  };
+  from: {
+    name: string;
+    building: string;
+    zone: string;
+    neighborhood: string;
+  };
+  to: {
+    name: string;
+    building: string;
+    zone: string;
+    neighborhood: string;
+  };
+  reward: string;
+  deletable: boolean;
+}
+
+interface FishAlbumEntry {
+  name: string;
+  weight: number;
+}
+
+interface Fish {
+  rod: {
+    id: number;
+    name: string;
+  };
+  collection: Record<
+    number,
+    {
+      name: string;
+      album: FishAlbumEntry[];
+    }
+  >;
+}
+
+interface Flower {
+  shovel: {
+    id: number;
+    name: string;
+  };
+  collection: Record<
+    number,
+    {
+      name: string;
+      album: string[];
+    }
+  >;
+}
+
+interface Cogsuit {
+  department: string;
+  hasDisguise: boolean;
+  suit: {
+    id: string;
+    name: string;
+  };
+  version: number;
+  level: number;
+  promotion: {
+    current: number;
+    target: number;
+  };
+}
+
+interface GolfStat {
+  name: string;
+  num: number;
+}
+
+interface RacingStat {
+  name: string;
+  num: number;
+}
+
+interface ToonData {
+  toon: Toon;
+  laff: {
+    current: number;
+    max: number;
+  };
+  location: Location;
+  gags: {
+    [key: string]: Gag | null;
+  };
+  tasks: Task[];
+  invasion: any;
+  fish: Fish;
+  flowers: Flower;
+  cogsuits: Record<string, Cogsuit>;
+  golf: GolfStat[];
+  racing: RacingStat[];
+}
