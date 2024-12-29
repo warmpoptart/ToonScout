6ae9ea1e4bf48037601344eab7dab7413383e445
erin.miller <erinm1018@gmail.com>
2024-12-28 19:01:42 -0600

    add: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { ConnectionProvider } from "./context/ConnectionContext";
 import { ToonProvider } from "./context/ToonContext";
+import ReactGA from "react-ga";
 import "./globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export const metadata: Metadata = {
   title: "ToonScout",
@@ -9,11 +12,30 @@ export const metadata: Metadata = {
     "Find and share your current Toontown Rewritten toon in Discord.",
 };
 
+ReactGA.initialize("G-SNDEYLE0QL");
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const trackPageview = (url: string) => {
+      ReactGA.pageview(url);
+    };
+
+    // Track the initial page load
+    trackPageview(window.location.pathname);
+
+    // Track subsequent route changes
+    router.events.on("routeChangeComplete", trackPageview);
+    return () => {
+      router.events.off("routeChangeComplete", trackPageview);
+    };
+  }, [router.events]);
+
   return (
     <html lang="en">
       <body className="font-impress text-gray-600">
