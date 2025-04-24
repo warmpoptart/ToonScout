afa135581d154a307d13f245253525ef8243a73b
Erin Miller <erinm1018@gmail.com>
2025-04-24 17:06:55 -0500

    modal: complete toon settings

--- app/components/Home/modals/ToonSettingsModal.tsx
+++ app/components/Home/modals/ToonSettingsModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "../../Modal";
 import { StoredToonData } from "@/app/types";
 import { useToonContext } from "@/app/context/ToonContext";
-import { FaLock, FaUnlock, FaCog } from "react-icons/fa";
+import { FaLock, FaUnlock, FaCog, FaTrash } from "react-icons/fa";
 
 
 type SettingsModalProps = {
@@ -13,7 +13,7 @@ type SettingsModalProps = {
 };
 
 const SettingsModal: React.FC<SettingsModalProps> = ({ toon, index, isOpen, onClose }) => {
-  if (!isOpen || !toon || !index) return null;
+  if (!isOpen || !toon || index == null) return null;
   const { toons, addToon } = useToonContext();
 
   const toggleLock = (index: number) => {
@@ -28,21 +28,36 @@ const SettingsModal: React.FC<SettingsModalProps> = ({ toon, index, isOpen, onCl
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <h3 className="text-3xl font-bold">{toon.data.data.toon.name}</h3>
+      <div className="px-2">
+        <h3 className="text-3xl font-bold">{toon.data.data.toon.name}</h3>
+        {/* individual settings */}
+        <div className="flex flex-col items-left gap-2 mb-4">
+          {/* lock */}
+          <div className="flex items-center gap-2">
+            <button
+              className="flex items-center gap-2 text-xl"
+              onClick={() => toggleLock(index)}>
+                {getLockedStatus(index) ? (
+                <FaUnlock className="text-green-500 text-2xl" />
+                ) : (
+                <FaLock className="text-red-500 text-2xl" />
+                )}
+            </button>
+            <span>Currently {getLockedStatus(index) ? "Unlocked" : "Locked"}</span>
+          </div>
+        </div>
 
-      {/* individual settings */}
-      <div>
-        {/* lock */}
-        <button
-          className="flex items-center gap-2 text-xl"
-          onClick={() => toggleLock(toons.indexOf(toon))}>
-          {getLockedStatus(toons.indexOf(toon)) ? (
-            <FaUnlock className="text-green-500" />
-          ) : (
-            <FaLock className="text-red-500" />
-          )}
-        </button>
         {/* deletion */}
+        <div className="flex items-center justify-center text-red-800">
+          <button
+            className="flex items-center gap-2 text-xl bg-red-200 p-2 rounded hover:bg-red-300"
+            onClick={() => {
+              deleteToon(index);
+            }}>
+            <FaTrash className="text-red-800" />
+            <span>Delete Toon</span>
+          </button>
+        </div>
       </div>
     </Modal>
   );
