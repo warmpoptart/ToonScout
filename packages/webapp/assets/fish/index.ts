cd024336b973ad755786ec1f1b420e22942e8d81
warmpoptart <128424622+warmpoptart@users.noreply.github.com>
2025-05-18 18:05:40 -0500

    cache(fish): move all the fishies images and make an index for the little guys

---
+++ packages/webapp/assets/fish/index.ts
@@ -0,0 +1,38 @@
+// Static imports for all fish images
+import amoreeel from "./amoreeel.png";
+import balloonfish from "./balloonfish.png";
+import bearacuda from "./bearacuda.png";
+import catfish from "./catfish.png";
+import clownfish from "./clownfish.png";
+import cuttthroat from "./cuttthroat.png";
+import devilray from "./devilray.png";
+import dog from "./dog.png";
+import frozenfish from "./frozenfish.png";
+import jellyfish from "./jellyfish.png";
+import kingcrab from "./kingcrab.png";
+import moonfish from "./moonfish.png";
+import nurseshark from "./nurseshark.png";
+import pianofish from "./pianofish.png";
+import poolshark from "./poolshark.png";
+import seahorse from "./seahorse.png";
+import starfish from "./starfish.png";
+
+export const fishImages = {
+  amoreeel,
+  balloonfish,
+  bearacuda,
+  catfish,
+  clownfish,
+  cuttthroat,
+  devilray,
+  dog,
+  frozenfish,
+  jellyfish,
+  kingcrab,
+  moonfish,
+  nurseshark,
+  pianofish,
+  poolshark,
+  seahorse,
+  starfish,
+};
